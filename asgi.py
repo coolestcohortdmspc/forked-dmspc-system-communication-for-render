a59f9c5e63549aec2229d86bf351042ce8aef10b
Desmond Lalli <dlalli@NRAO.com>
2026-09-09 17:40:07 -0400

    Working through errors with FastAPI/Django integration

--- asgi.py
+++ asgi.py
@@ -12,7 +12,7 @@ import os
 from django.core.asgi import get_asgi_application
 
 from fastapi.middleware.wsgi import WSGIMiddleware
-from fastapi_app import app as fastapi_app
+from views import app as fastapi_app
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 
