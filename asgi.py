6001cc6a08938e2d850abafbb600f5b60367f012
tnguyen-nrao <tnguyen@nrao.edu>
2026-09-10 15:35:25 -0400

    implement basic fastapi async

--- asgi.py
+++ asgi.py
@@ -8,16 +8,13 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
-
-from fastapi.middleware.wsgi import WSGIMiddleware
 from fastapi_app import app as fastapi_app
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 
 django_application = get_asgi_application()
 
-#create an asynchronous sever which works with both Django and FastAPI
-aync_app = WSGIMiddleware(django_application)
-async_app.mount("/api", fastapi_app)
+#create an asynchronous server which works with both Django and FastAPI
+application = fastapi_app
+application.mount("/", django_application)
